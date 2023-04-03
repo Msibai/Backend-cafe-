4bc622d94239eb10a2a201ae581796aea7fa3b83
ShahdMardelli <shahdmardeli@gmail.com>
2023-04-03 11:06:59 +0200

    Buttons replaced with links in AdminDashboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
+	BrowserRouter,
 	createBrowserRouter,
 	createRoutesFromElements,
 	Route,
@@ -44,6 +45,6 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+	<RouterProvider router={router} />	
 	</React.StrictMode>
 );
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
-	return (
-		<div>
-		  <h1>Admin Dashboard</h1>
-		  <div className="button-container">
-			<button onClick={() => { window.location.href = '/addMenu' }}>Add Menu</button>
-			<button onClick={() => { window.location.href = '/updateMenu' }}>Update Menu</button>
-		  </div>
-		</div>
-	  )
-	
-  };
-  
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <div className="button-container">
+        <Link to="/addMenu">Add Menu</Link>
+        <Link to="/updateMenu">Update Menu</Link>
+      </div>
+    </div>
+  );
+};
 
 export default AdminDashboard;
+
+
