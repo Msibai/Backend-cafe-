03a18f0f3d83aebc60d5504d29559f4becf0d829
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-04-05 16:36:20 +0200

    admin-dashboard-css

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,17 +1,20 @@
 import "./css/adminDashboard.css";
+import Loading from "../images/loading3.gif";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
   return (
     <div className="admin-dash-container">
-      <h1>Admin Dashboard</h1>
+      <h1 className="admin-dash-title">Admin Dashboard</h1>
+      <p className="admin-intro-text">Make changes to your menu</p>
+      <img src={Loading} className="loading-gif"></img>
       <div className="button-container">
         <Link to="/dashboard/addmenu" className="admin-link-buttons">
-          Add Menu
+          <button className="admin-buttons">Add a menu item</button>
         </Link>
         <Link to="/dashboard/updatemenu" className="admin-link-buttons">
-          Update Menu
+          <button className="admin-buttons">Update a menu item</button>
         </Link>
       </div>
     </div>
