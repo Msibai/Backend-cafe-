7e753eafb3f9751d258bf61b25783caa25bba377
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-04-06 11:57:08 +0200

    resolved conflicts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,33 +21,8 @@ import AddMenu from './pages/addMenu';
 import UpdateMenu from './pages/UpdateMenu';
 import UpdateMenuItem from './pages/UpdateMenuItem';
 import AboutUs from './pages/AboutUs';
-<<<<<<< HEAD
-import DeleteMenuItem from "./pages/DeleteMenuItem";
-
-const router = createBrowserRouter(
-  createRoutesFromElements([
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="menu" element={<Menu />}>
-        <Route path=":menuid" element={<MenuItem />} />
-      </Route>
-      <Route path="signup" element={<SignUp />} />
-      <Route path="signin" element={<SignIn />} />
-      <Route path="orders" element={<OredersStatus />}>
-        <Route path=":orderid" element={<Oreder />} />
-      </Route>
-      <Route path="dashboard" element={<AdminDashboard />} />
-      <Route path="/dashboard/addmenu" element={<AddMenu />} />
-      <Route path="/dashboard/updatemenu" element={<UpdateMenu />} />
-      <Route path="/dashboard/update-menu-item/:id"element={<UpdateMenuItem />} />
-      <Route path="/dashboard/delete-menu-item/:id" element={<DeleteMenuItem />}/>
-      
-      <Route path='aboutUs' element={<AboutUs />} />
-      
-    </Route>,
-  ])
-=======
 import MyAccount from './pages/MyAccount';
+import DeleteMenuItem from './pages/DeleteMenuItem';
 
 const router = createBrowserRouter(
 	createRoutesFromElements([
@@ -65,14 +40,14 @@ const router = createBrowserRouter(
 			<Route path='/dashboard/addmenu' element={<AddMenu />} />
 			<Route path='/dashboard/updatemenu' element={<UpdateMenu />} />
 			<Route
-				path='/dashboard/update-menu-item/:id'
-				element={<UpdateMenuItem />}
-			/>
+				path='/dashboard/update-menu-item/:id'element={<UpdateMenuItem />}/>
+        <Route path="/dashboard/delete-menu-item/:id" element={<DeleteMenuItem />}/>
+			
 			<Route path='aboutUs' element={<AboutUs />} />
 			<Route path='myaccount' element={<MyAccount />} />
 		</Route>,
 	])
->>>>>>> main
+
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
