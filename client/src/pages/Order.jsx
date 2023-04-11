667d9d33a5c5862ec2c00ae70437c3b371f59b82
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-04-11 19:25:12 +0200

    Trying to implement orders

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,4 +1,44 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "../style/handle-orders.css";
+
 function Order() {
-	return <h1>Order</h1>;
+  const [order, setOrder] = useState();
+  const [items, setItems] = useState();
+  const [customer, setCustomer] = useState();
+  const [orderNumber, setOrderNumber] = useState();
+
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
+
+  const params = useParams();
+
+  function goBack() {
+    navigate("/orders");
+  }
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const response = await fetch(`/api/orders/${params.id}`);
+      setOrder(await response.json());
+      console.log(order);
+      console.log(response);
+    };
+
+    fetchItem();
+  }, [params.id]);
+
+  return (
+    <div>
+      <div className="row">
+        <div className="cell">Customer Name</div>
+        <div className="cell">Items ordered</div>
+      </div>
+      <div className="row">
+        <div className="cell">{order?.name}</div>
+        <div className="cell">{order?.items}</div>
+      </div>
+    </div>
+  );
 }
 export default Order;
--- client/src/pages/OrdersStatus.jsx
+++ client/src/pages/OrdersStatus.jsx
@@ -1,4 +1,40 @@
+import "../style/handle-orders.css";
+import { useState, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
+
 function OredersStatus() {
-	return <h1>Orders</h1>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const response = await fetch("/api/orders");
+      const data = await response.json();
+      setOrders(data);
+    };
+
+    fetchOrders();
+  }, []);
+
+  return (
+    <div>
+      <Outlet />
+      <h1 className="handle-orders-title">Handle Orders</h1>
+      <div className="handle-orders-table">
+        <div className="row">
+          <div className="cell">Order Id</div>
+        </div>
+        {orders.map((order) => (
+          <div key={order._id} className="row">
+            <div className="cell">{order._id}</div>
+
+            <Link to={`/orders/${order._id}`} state={{ data: order._id }}>
+              <button className="handle-order-button">Handle order</button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default OredersStatus;
