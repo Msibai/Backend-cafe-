cd1f8e5b0d01d327c0b92754ac121f0d7a724e18
Mukhtar Sibai <mukhtar@Mukhtars-MacBook-Pro.local>
2023-04-12 21:41:14 +0200

    Fixing session bug

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -4,55 +4,67 @@ import { useNavigate } from 'react-router-dom';
 const GlobalContext = createContext(null);
 
 export const GlobalProvider = ({ children }) => {
-	// useState for all variables
 	const authFromSession = sessionStorage.getItem('auth') || false;
 	const isadminFromSession = sessionStorage.getItem('isadmin') || false;
 	const isworkerFromSession = sessionStorage.getItem('isworker') || false;
 	const iscustomerFromSession = sessionStorage.getItem('iscustomer') || false;
 	const userIdFromSession = sessionStorage.getItem('userId') || '';
+	const userNameFromSession = sessionStorage.getItem('userName') || '';
 
 	const [auth, setAuth] = useState(authFromSession);
 	const [isworker, setIsworker] = useState(isworkerFromSession);
 	const [isadmin, setIsadmin] = useState(isadminFromSession);
 	const [iscustomer, setIscustomer] = useState(iscustomerFromSession);
-	const [user, setUser] = useState();
+	const [user, setUser] = useState(userNameFromSession);
 	const [userId, setUserId] = useState(userIdFromSession);
 	const [menus, setMenus] = useState([]);
 
 	const navigate = useNavigate();
 
 	const submitLogin = async (email, password) => {
-		const response = await fetch('/api/login', {
-			method: 'post',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email, password }),
-		});
-		const result = await response.json();
-		if (result) {
-			setAuth(true);
-			setUser(result.user.name);
-			setUserId(result.user._id);
-			sessionStorage.setItem('User', user);
-			sessionStorage.setItem('auth', auth);
-			sessionStorage.setItem('userId', userId);
-		}
+		try {
+			const response = await fetch('/api/login', {
+				method: 'post',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ email, password }),
+			});
+			const result = await response.json();
+			if (result) {
+				setAuth(true);
+				setUser(result.user.name);
+				setUserId(result.user._id);
+			}
+			if (result.user.admin) {
+				setIsadmin(true);
 
-		if (result.user.admin) {
-			setIsadmin(true);
-			sessionStorage.setItem('isadmin', isadmin);
+				navigate('/dashboard');
+			} else if (result.user.restaurantWorker) {
+				setIsworker(true);
 
-			navigate('/dashboard');
-		} else if (result.user.restaurantWorker) {
-			setIsworker(true);
-			sessionStorage.setItem('isworker', isworker);
-
-			navigate('');
-		} else if (!result.user.admin && !result.user.restaurantWorker) {
-			setIscustomer(true);
-			sessionStorage.setItem('iscustomer', iscustomer);
-			navigate('/myaccount', { state: { id: result.user._id } });
+				navigate('');
+			} else if (!result.user.admin && !result.user.restaurantWorker) {
+				setIscustomer(true);
+				navigate('/myaccount');
+			}
+		} catch (error) {
+			console.log(error); // check if there are any errors
 		}
 	};
+
+	useEffect(() => {
+		if (auth) {
+			sessionStorage.setItem('userName', user);
+			sessionStorage.setItem('auth', auth);
+			sessionStorage.setItem('userId', userId);
+			if (isadmin) {
+				sessionStorage.setItem('isadmin', isadmin);
+			} else if (isworker) {
+				sessionStorage.setItem('isworker', isworker);
+			} else {
+				sessionStorage.setItem('iscustomer', iscustomer);
+			}
+		}
+	}, [auth, isadmin, iscustomer, isworker, userId]);
 
 	const logout = async () => {
 		const response = await fetch('/api/login', {
@@ -60,6 +72,11 @@ export const GlobalProvider = ({ children }) => {
 		});
 		const result = await response.json();
 		setAuth(false);
+		setIsadmin(false);
+		setIsworker(false);
+		setIscustomer(false);
+		setUserId('');
+		setUser('');
 		sessionStorage.clear();
 	};
 
