dfdf3b3d49d1caec4a047a944174ea7c234b26d6
Niklas Affenita <115690023+NickPA98@users.noreply.github.com>
2023-04-12 16:46:46 +0200

    more-css-changes

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,39 +1,55 @@
-import React from "react"
-import "../style/aboutUs.css"
-
-
+import React from "react";
+import "../style/aboutUs.css";
+import Gradient from "../images/gradient.jpg";
 
 function AboutUs() {
   return (
-    <div className="about-us-container">
-      <div className="about-us-header">
-        <h1>Discover Our Story</h1>
-        <p className="about-us-description">
-          At Back End Café, we believe that coffee and coding go hand in hand. Just as coffee is the fuel that keeps you going, programming is the brainpower that fuels your creativity and innovation. Our menu features a variety of coffee blends, each with its unique taste and aroma, carefully selected to inspire and invigorate your senses.
-        </p>
-      </div>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${Gradient})` }}
+    >
+      <div className="about-us-container">
+        <div className="about-us-header">
+          <h1>Discover Our Story</h1>
+          <p className="about-us-intro">
+            At Back End Café, we believe that coffee and coding go hand in hand.
+            Just as coffee is the fuel that keeps you going, programming is the
+            brainpower that fuels your creativity and innovation. Our menu
+            features a variety of coffee blends, each with its unique taste and
+            aroma, carefully selected to inspire and invigorate your senses.
+          </p>
+        </div>
 
-      <div className="about-us-body">
-        <h2>The Perfect Blend</h2>
-        <p className="about-us-description">
-          Our café is designed with a programming theme, with cozy seating arrangements that resemble a coding workstation, complete with desks, monitors, and keyboard trays. Whether you're looking for a quiet place to work on your coding project or want to discuss the latest tech trends with like-minded individuals, Back End Café is the perfect place to be.
-        </p>
+        <div className="about-us-body">
+          <h2>The Perfect Blend</h2>
+          <p className="about-us-description">
+            Our café is designed with a programming theme, with cozy seating
+            arrangements that resemble a coding workstation, complete with
+            desks, monitors, and keyboard trays. Whether you're looking for a
+            quiet place to work on your coding project or want to discuss the
+            latest tech trends with like-minded individuals, Back End Café is
+            the perfect place to be.
+          </p>
 
-        <h2>Knowledgeable Staff</h2>
-        <p className="about-us-description">
-          Our staff is knowledgeable, friendly, and passionate about both coffee and coding. They are always happy to offer recommendations on our coffee blends, as well as tips and tricks on coding techniques and tools.
-        </p>
+          <h2>Knowledgeable Staff</h2>
+          <p className="about-us-description">
+            Our staff is knowledgeable, friendly, and passionate about both
+            coffee and coding. They are always happy to offer recommendations on
+            our coffee blends, as well as tips and tricks on coding techniques
+            and tools.
+          </p>
 
-        <h2>A Community of Tech Enthusiasts</h2>
-        <p className="about-us-description">
-          So, come on in and enjoy a cup of coffee with a side of coding inspiration. At Back End Café, we're more than just a coffee shop; we're a community of tech enthusiasts who share a love for programming and coffee.
-        </p>
+          <h2>A Community of Tech Enthusiasts</h2>
+          <p className="about-us-description">
+            So, come on in and enjoy a cup of coffee with a side of coding
+            inspiration. At Back End Café, we're more than just a coffee shop;
+            we're a community of tech enthusiasts who share a love for
+            programming and coffee.
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default AboutUs;
-
-
-   
--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import "../style/footer.css"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import "../style/footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-links">
-      <Link to="/aboutUs" target="_blank">About us</Link>
+        <Link to="/aboutUs" target="_blank">
+          About us
+        </Link>
         <div className="social-links">
-        <a href="https://www.facebook.com" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
-          <a href="https://www.instagram.com" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a>
-          <a href="https://www.twitter.com" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="https://www.facebook.com" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://www.instagram.com" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://www.twitter.com" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
         </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
-
